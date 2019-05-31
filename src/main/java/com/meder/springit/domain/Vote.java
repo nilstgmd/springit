2cb42f05bf810490f05def5257144512b8a700bc
Nils Meder <meder@adobe.com>
2019-05-31 19:06:33 +0200

    Create Domain Model

---
+++ src/main/java/com/meder/springit/domain/Vote.java
@@ -0,0 +1,35 @@
+/*************************************************************************
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ **************************************************************************/
+
+package com.meder.springit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Vote {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private int vote;
+}
